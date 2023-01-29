6b599c77e8f9c068094713ccca0d5a6d2bf5eb5b
iprijic <iprijic21@gmail.com>
2023-01-29 02:13:55 +0100

    Finished primary form design.

--- MyRent.Blazor.WebSite/Component/FormDetail.cs
+++ MyRent.Blazor.WebSite/Component/FormDetail.cs
@@ -15,9 +15,37 @@ namespace MyRent.Blazor.Web.Components
     {
         [Parameter]
         public EventCallback OnShowTableHandler { get; set; }
+
+        [Parameter]
+        public Nullable<Int64> ID { get; set; }
+
+        [Parameter]
+        public List<Dictionary<String, Object>> Rows { get; set; }
+
+        private Dictionary<String, Object> selected;
+
+
         public async Task OnBack(MouseEventArgs e)
         {
             await OnShowTableHandler.InvokeAsync();
+        }
+
+        private void SelectRow()
+        {
+            foreach(Dictionary<String, Object> row in Rows)
+            {
+                if(((JsonElement)row["ID"]).GetInt64() == ID)
+                {
+                    selected = row;
+                    break;
+                }
+            }
+        } 
+
+        protected override Task OnInitializedAsync()
+        {
+            SelectRow();
+            return base.OnInitializedAsync();
         }
     }
 }
